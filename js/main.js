df6a179d6616bada8a26dc7310585ba4c8305a08
JZamora11 <jzamora11p@gmail.com>
2020-10-24 17:55:56 -0500

    JZ Producciones v0.5.2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
-/* NAVBAR */
+/* TYPED */
+const typed = new Typed('.typed', {
+    strings: [
+        '<b>Juan Zamora</b>',
+        '<b>Editor de vídeo</b>',
+        '<b>Fotógrafo</b>',
+        '<b>Diseñador web</b>',
+        '<b>Ilustrador</b>'
+    ],
+	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 500, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
+
+/* VER VIDEO */
+$(document).ready(function(e){
+    $('.btn1').on('mouseenter', function(e){
+        x = e.pageX - $(this).offset().left;
+        y = e.pageY - $(this).offset().top;
+        $(this).find('span').css({top: y, left: x})
+    })
+    $('.btn1').on('mouseout', function(e){
+        x = e.pageX - $(this).offset().left;
+        y = e.pageY - $(this).offset().top;
+        $(this).find('span').css({top: y, left: x})
+    })
+})
